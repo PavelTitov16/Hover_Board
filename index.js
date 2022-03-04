a14355906a81105969783df050583a103ec36cb8
PavelTitov16 <pashoktitov16@gmail.com>
2022-03-04 21:45:42 +0300

    ready app

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const board = document.querySelector('#board');
-const colors = ['#DE3218', '#F19314', '#F1E014', '#1BC51F', '#0B8DCF', '#0732BF', '#7B0AA4']
+const colors = ['#DE3218', '#F19314', '#F1E014', '#1BC51F', '#0B8DCF', '#0732BF', '#7B0AA4'];
 const squaresNumbers = 500;
 
 for (let i = 0; i < squaresNumbers; i++) {
@@ -16,13 +16,13 @@ function setColor(event) {
     const element = event.target;
     const color = getRandomColor();
     element.style.backgroundColor = color;
-    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 }
 
 function removeColor(event) {
     const element = event.target;
     element.style.backgroundColor = '#AAA5A5';
-    element.style.boxShadow = `0 0 2px #000`
+    element.style.boxShadow = `0 0 2px #000`;
 }
 
 function getRandomColor() {
